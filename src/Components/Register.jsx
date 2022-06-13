d4f3b269a3ff3792064795ffeb9c08502e54b82f
safaziedi <safaziedi070@gmail.com>
2022-06-13 09:42:20 +0100

    delete local Storage

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -5,44 +5,11 @@ import email from "./../image/email.jpg";
 import passWORD from "./../image/pass.png";
 import Users from "./Users.jsx";
 
-
-
-// getting the values of local storage
-const getDatafromLS = () => {
-  const data = localStorage.getItem("users");
-  if (data) {
-    return JSON.parse(data);
-  } else {
-    return [];
-  }
-};
-
 const Register = () => {
-  const [users, setUsers] = useState(getDatafromLS());
-
-  const [name, setName] = useState("");
-  const [pass, setPass] = useState("");
-
-  const handleAddUserSubmit = (e) => {
-    e.preventDefault();
-    let user = {
-      name,
-      pass,
-    };
-
-    setUsers([...users, user]);
-    setName("");
-    setPass("");
-  };
-
-  // saving data to local storage
-  useEffect(() => {
-    localStorage.setItem("users", JSON.stringify(users));
-  }, [users]);
 
   return (
     <div>
-      <form onSubmit={handleAddUserSubmit}>
+      <form>
         <div className="main">
           <div className="sub-main">
             <div>
@@ -60,8 +27,6 @@ const Register = () => {
                     placeholder="user name"
                     className="name"
                     required="required"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
                   />
                 </div>
                 <div className="second-input">
@@ -71,8 +36,6 @@ const Register = () => {
                     placeholder="user password"
                     className="name"
                     required="required"
-                    onChange={(e) => setPass(e.target.value)}
-                    value={pass}
                   />
                 </div>
                 <div className="login-button">
@@ -83,25 +46,6 @@ const Register = () => {
           </div>
         </div>
       </form>
-
-      <div className='Users-container'>
-          {users.length>0&&<>
-            <div className='table-responsive'>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th>Name </th>
-                    <th>Password </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <Users users={users}/>
-                </tbody>
-              </table>
-            </div>
-          </>}
-          {users.length < 1 && <div>No users are added yet</div>}
-        </div>
 
       </div>
    
